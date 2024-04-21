bc237dfac7767b1201a601312b99ef566fe3b74b
JojoGP215 <jojogp215@outlook.com>
2024-04-21 15:41:30 +0200

    Grundlagen erstellt
    Sachen Installiert, Controller erstellt und RacingActionController genannt

---
+++ src/Controller/RacingActionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RacingActionController extends AbstractController
+{
+    #[Route('/racing/action', name: 'app_racing_action')]
+    public function index(): Response
+    {
+        return $this->render('racing_action/index.html.twig', [
+            'controller_name' => 'RacingActionController',
+        ]);
+    }
+}
