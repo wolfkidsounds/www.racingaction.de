a94d313a1d093635591a07d1619c015ae5b270c3
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-04-25 05:55:08 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ Encore
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+        pattern: /\.(png|jpg|jpeg)$/
+    })
+
     /*
      * ENTRY CONFIG
      *
