655552052e8b1212e325f71795e377e124fc458b
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-02 01:02:26 +0200

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -16,41 +16,88 @@ class IndexController extends AbstractController
      * Besucher können hier einen Kalender sehen (?)
      * 
      * Sichtbarkeit: Öffentlich
+     * Status: Template
      *
      * @return Response
      */
     #[Route('/', name: 'index')]
     public function index(): Response
     {
+        // $events = $repository->findAll();
+
         //TODO: Kalender in das Template schicken
         return $this->render('public/index.html.twig', [
-            'controller_name' => 'IndexController',
-            // 'calender' => $calendar,
+            // 'events' => $events,
         ]);
     }
 
     /**
-     * Organizer (Übersicht?)
+     * Tracks
      * 
+     * Anzahl: Mehrere (Übersicht)
      * Sichtbarkeit: Öffentlich
+     * Status: Template
      *
      * @return Response
      */
-    #[Route('/organizer', name: 'organizer')]
-    public function organizer(): Response
+    #[Route('/tracks', name: 'tracks')]
+    public function tracks(): Response
     {
-        // JJ: öffentliche Seite
-        // JJ: in templates abgelegt
-        // JJ: ich würde alles in einzahl bennenen
-        return $this->render('public/organizer/index.html.twig', [
-            'controller_name' => 'IndexController',
+        //TODO: $tracks = $repository->findAll();
+
+        return $this->render('public/tracks.html.twig', [
+            // 'tracks' => $tracks,
         ]);
     }
 
     /**
-     * Rider (Übersicht?) JJ: Ja, Daten nach Öffentlichkeitsstatus. Wenn Rider (privat) ausgewählt, dann wird er in der Liste mit "FAHRER" angezeigt
+     * Organizer
      * 
+     * Anzahl: Mehrere (Übersicht)
      * Sichtbarkeit: Öffentlich
+     * Status: Template
+     *
+     * @return Response
+     */
+    #[Route('/organizers', name: 'organizers')]
+    public function organizers(): Response
+    {
+        // $organizers = $repository->findAll();
+
+        return $this->render('public/organizers.html.twig', [
+            // 'organizers' => $organizers,
+        ]);
+    }
+
+    /**
+     * Brands
+     * 
+     * Anzahl: Mehrere (Übersicht)
+     * Sichtbarkeit: Öffentlich
+     * Status: Template
+     *
+     * @return Response
+     */
+    #[Route('/brands', name: 'brands')]
+    public function brands(): Response
+    {
+        // $brands = $repository->findAll();
+
+        return $this->render('public/brands.html.twig', [
+            // 'brands' => $brands,
+        ]);
+    }
+
+    /**
+     * Rider (Übersicht?) 
+     * 
+     * JJ: Ja, Daten nach Öffentlichkeitsstatus.
+     * Wenn Rider (privat) ausgewählt, 
+     * dann wird er in der Liste mit "FAHRER" angezeigt
+     * 
+     * Anzahl: Mehrere (Übersicht)
+     * Sichtbarkeit: Öffentlich
+     * Status: TODO:
      *
      * @return Response
      */
@@ -60,8 +107,11 @@ class IndexController extends AbstractController
         //TODO: Bestimmen ob das eine Private oder Öffentliche Seite ist?
         // entsprechends template wählen (public/rider.html.twig)?
         // einzahl oder mehrzahl? (riders -> viele)
+
+        // $riders = $repository->findBy('Status', RiderStatus::PUBLIC);
+
         return $this->render('seiten/user/rider/index.html.twig', [
-            'controller_name' => 'IndexController',
+            // 'riders' => $riders,
         ]);
     }
 
@@ -69,17 +119,35 @@ class IndexController extends AbstractController
      * Event (Übersicht?)
      * 
      * Sichtbarkeit: Öffentlich
+     * Status: TODO:
      *
      * @return Response
      */
-    #[Route('/event', name: 'event')]
+    #[Route('/events', name: 'events')]
     public function event(): Response
     {
         //TODO: Bestimmen ob das eine Private oder Öffentliche Seite ist?
         // entsprechends template wählen (public/event.html.twig)?
         // einzahl oder mehrzahl? (evnts -> viele)
+
+        // $events = $repository->findBy('Status', EventStatus::PUBLIC);
         return $this->render('seiten/calendar/event/index.html.twig', [
-            'controller_name' => 'IndexController',
+            // 'events' => $events,
         ]);
+    }
+
+    /**
+     * About
+     * 
+     * Anzahl: /
+     * Sichtbarkeit: Öffentlich
+     * Status: Template
+     *
+     * @return Response
+     */
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('public/about.html.twig');
     }
 }
