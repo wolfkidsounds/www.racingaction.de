14f386dcb9c6a7fd1f0ecbe787687201949b23de
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-02 01:51:27 +0200

    Create AdminController.php

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php // src/Controller/Admin/AdminController.php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin', name: 'admin_')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('admin/pages/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
