a74f853d39a243c79b9db9e9a324578e642558eb
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-02 01:49:28 +0200

    Create admin.js

---
+++ assets/admin.js
@@ -0,0 +1,11 @@
+/*  admin.js
+*   Diese Datei lädt Inhalte speziell für Admin Seiten.
+*/
+
+import './bootstrap.js'; // Stimulus
+import '@popperjs/core';
+import 'bootstrap'; // Bootstrap
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import './js/admin.js'; // JavaScript
+import './scss/admin.scss'; // SASS/CSS
