fbdc3e489ed5051e01a9dcafbf5dd08de28e0ac7
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-04 19:04:53 +0200

    Update AdminController.php

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('admin/pages/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
