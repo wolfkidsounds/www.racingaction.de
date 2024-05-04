ff4441f7e287ad1c14097e96fcc03322b8d7c1b0
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-05 00:14:22 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,10 @@
+@echo off
+
 ::installiert composer pakete
 call composer install
 
-::installier node pakete
+::installiert node pakete
 call npm install
+
+:: database migrations
+call php bin/console --no-interaction doctrine:migrations:migrate
