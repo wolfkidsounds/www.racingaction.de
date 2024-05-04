ec8c2b7f41ba57f355dc1f5bfa1b04f7ac635f16
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-04 19:04:57 +0200

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NICKNAME', fields: ['Nickname'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
