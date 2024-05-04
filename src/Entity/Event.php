3411877f9857bf7174838cc07095625b2f9fe9da
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-04 18:16:12 +0200

    Create Event.php

---
+++ src/Entity/Event.php
@@ -0,0 +1,20 @@
+<?php // src/Entity/Event.php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
