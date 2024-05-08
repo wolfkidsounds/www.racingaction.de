131cdeacde6905a75c1aa7c79a9b8501e39a801e
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-08 02:06:01 +0200

    Create EventFormType.php

---
+++ src/Form/Admin/EventFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\Event;
+use Doctrine\DBAL\Types\BooleanType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class EventFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('dateTimeBegin', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('dateTimeEnd', DateTimeType::class, [
+                'required' => false,
+                'widget' => 'single_text',
+            ])
+            ->add('isOpenEnd', CheckboxType::class, [
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
