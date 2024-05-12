de10c39babf944d0c5bb3426dbaea933a8ea96f2
JojoGP215 <jojogp215@outlook.com>
2024-05-12 20:58:06 +0200

    components erstellt

---
+++ src/Twig/Components/Admin/Card.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Twig\Components\Admin;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Card
+{
+    /**
+     * Undocumented variable
+     * ? für kann auch leer sein
+     * typ: array
+     * name: $items
+     * wert: null
+     *
+     * @var array|null
+     */
+    public ?array $item = [];
+}
