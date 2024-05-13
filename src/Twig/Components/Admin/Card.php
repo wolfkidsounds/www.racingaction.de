7bcc65e4353d11c129b3fe786cb34b39e10edd34
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-13 05:15:10 +0200

    Update Card.php

--- src/Twig/Components/Admin/Card.php
+++ src/Twig/Components/Admin/Card.php
@@ -7,15 +7,6 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class Card
 {
-    /**
-     * Undocumented variable
-     * ? für kann auch leer sein
-     * typ: array
-     * name: $items
-     * wert: null
-     *
-     * @var array|null
-     */
     public ?array $item = [];
 
     public ?string $style = 'primary';
