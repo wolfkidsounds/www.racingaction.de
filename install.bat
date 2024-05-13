f19ca1eb44ad6fb92c740fe2fa64f1ada9189f2b
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-13 07:15:45 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,6 +4,9 @@
 call composer install --no-interaction
 ::call composer update --no-interaction
 
+::installiert php flasher
+call php bin/console flasher:install
+
 ::installiert node pakete und bringt sie auf den neusten stand
 call npm install --no-interaction
 ::call npm update --no-interaction
