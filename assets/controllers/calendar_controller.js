f006098c85bc5c6c493706aed389f75fbd5d8e64
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-26 15:21:27 +0200

    Update calendar_controller.js

--- assets/controllers/calendar_controller.js
+++ assets/controllers/calendar_controller.js
@@ -1,12 +1,25 @@
 import { Controller } from '@hotwired/stimulus';
-import { Calendar } from 'fullcalendar';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import allLocales from '@fullcalendar/core/locales-all';
 
 export default class extends Controller {
     connect() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new Calendar(calendarEl, {
+        let calendarEl = document.getElementById('calendar');
+        let calendar = new Calendar(calendarEl, {
+            plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
             initialView: 'dayGridMonth',
-            themeSystem: 'bootstrap5',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,listWeek'
+            },
+            firstDay: 1,
+            locales: allLocales,
+            locale: 'de',
+            events: '/_api/calendar/feed'
         });
 
         calendar.render();
