ab74fb4ddd0f30db853a9e9c0db067d396a8fca7
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-28 02:17:09 +0200

    Update EventRepository.php

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -2,9 +2,10 @@
 
 namespace App\Repository;
 
+use DateTime;
 use App\Entity\Event;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Event>
@@ -14,6 +15,18 @@ class EventRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Event::class);
+    }
+
+    public function findBetweenDates(DateTime $start, DateTime $end): array
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.dateStart >= :start')
+            ->andWhere('e.dateEnd <= :end')
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->getQuery()
+            ->getResult()
+        ;
     }
 
     //    /**
