20df0a95390cdaaeba8381c5b5bde32f2d5bad33
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-29 16:27:06 +0200

    Update LocationType.php

--- src/Form/Event/LocationType.php
+++ src/Form/Event/LocationType.php
@@ -4,7 +4,9 @@ namespace App\Form\Event;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,12 +21,17 @@ class LocationType extends AbstractType
                 'required' => true,
             ])
 
-            ->add('url', TextType::class, [
-                'label' => 'Link',
+            ->add('address', TextType::class, [
+                'label' => 'Address',
                 'required' => true,
             ])
 
-            ->add('description', TextType::class, [
+            ->add('url', UrlType::class, [
+                'label' => 'Link',
+                'required' => false,
+            ])
+
+            ->add('description', TextareaType::class, [
                 'label' => 'Description',
                 'required' => false,
             ])
