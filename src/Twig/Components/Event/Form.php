46bedbc3112d65a1978ca08dfc72fd7ca2d383cd
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-05-29 14:55:57 +0200

    Update Form.php

--- src/Twig/Components/Event/Form.php
+++ src/Twig/Components/Event/Form.php
@@ -5,73 +5,27 @@ namespace App\Twig\Components\Event;
 use DateTime;
 use App\Entity\Event;
 use DateTimeInterface;
+use Symfony\Component\Form\FormView;
+use App\Form\Event\CreateEventFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[AsLiveComponent]
 final class Form extends AbstractController
 {
     use DefaultActionTrait;
+    use ComponentWithFormTrait;
 
     #[LiveProp(writable: true)]
-    public ?Event $event = null;
-    public $entity = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $name = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $type = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $location = null;
-
-    #[LiveProp(writable: true)]
-    public ?DateTimeInterface $dateStart = null;
-
-    #[LiveProp(writable: true)]
-    public ?DateTimeInterface $dateEnd = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $priceVisitor = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $priceRider = null;
-
-    #[LiveProp(writable: true)]
-    public ?DateTime $dateTimeStartVisitor = null;
-
-    #[LiveProp(writable: true)]
-    public ?DateTime $dateTimeArriving = null;
-
-    #[LiveProp(writable: true)]
-    public ?DateTime $dateTimeRidersbreefing = null;
-
-    #[LiveProp(writable: true)]
-    public ?DateTime $dateTimeLeaving = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $classes = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $registration = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $linkUrl1 = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $linkUrl2 = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $linkUrl3 = null;
-
-    #[LiveProp(writable: true)]
-    public ?string $description = null;
+    public ?Event $initialFormData = null;
 
     #[LiveProp(writable: true)]
     public ?bool $isEditing = false;
@@ -82,33 +36,25 @@ final class Form extends AbstractController
         $this->isEditing = true;
     }
 
-    #[LiveAction]
-    public function save(EntityManagerInterface $em, ?bool $ansehen): Response
+    protected function instantiateForm(): FormInterface
     {
-        $this->isEditing = false;
+        // we can extend AbstractController to get the normal shortcuts
+        return $this->createForm(CreateEventFormType::class, $this->initialFormData);
+    }
 
-        $event = new Event(); // Neues Objekt erzeugen
-        $event->setName($this->name);
-        $event->setType($this->type);
-        $event->setLocation($this->location);
-        $event->setDateStart($this->dateStart);
-        $event->setDateEnd($this->dateEnd);
-        $event->setRegistration($this->registration);
-        $event->setDescription($this->description); // Objekt mit infos füllen
-        $event->setLinks([
-            $this->linkUrl1, 
-            $this->linkUrl2, 
-            $this->linkUrl3,
-        ]);
+    #[LiveAction]
+    public function save(EntityManagerInterface $em)
+    {
+        // Submit the form! If validation fails, an exception is thrown
+        // and the component is automatically re-rendered with the errors
+        $this->submitForm();
 
-        $em->persist($event); // Befehl zum Speichern
-        $em->flush(); // Alle Befehle Ausführen
-        
-        noty()->success("Super! Das hat funktioniert");
+        /** @var Post $post */
+        $post = $this->getForm()->getData();
+        $em->persist($post);
+        $em->flush();
 
-        if ($ansehen == true) {
-            return $this->redirectToRoute('app_events_read');
-        }
+        noty()->success('Saved successfully');
 
         return $this->redirectToRoute('app_events_index');
     }
