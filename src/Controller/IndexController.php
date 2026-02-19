1672535947e875f64828fe07eaa4bb6c7c5bb07d
JojoGP215 <jojogp215@outlook.com>
2026-02-19 16:52:02 +0100

    eventpage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,7 +14,7 @@ final class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
+        return $this->render('pages/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
     }
