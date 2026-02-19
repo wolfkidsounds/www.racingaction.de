8ab561b0ed60f9253c4990fe1f490fbd0d005840
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2026-02-19 19:25:30 +0100

    Update EventLink, add exampels

--- src/Twig/Components/EventLink.php
+++ src/Twig/Components/EventLink.php
@@ -5,4 +5,42 @@ namespace App\Twig\Components;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent()]
-final class EventLink {}
+final class EventLink
+{
+    public string $url;
+    public int $size = 32;
+    public bool $withIcon = true;
+
+    public function getDomain(): string
+    {
+        if (!$this->url) {
+            return '';
+        }
+
+        if (!str_starts_with($this->url, 'http')) {
+            $this->url = 'https://' . $this->url;
+        }
+
+        $host = parse_url($this->url, PHP_URL_HOST);
+
+        if (!$host) {
+            return '';
+        }
+
+        // Entfernt www.
+        return preg_replace('/^www\./', '', $host);
+    }
+
+    public function getIconUrl(): string
+    {
+        if (!$this->getDomain()) {
+            return '';
+        }
+
+        return sprintf(
+            'https://www.google.com/s2/favicons?domain=%s&sz=%d',
+            $this->getDomain(),
+            $this->size
+        );
+    }
+}
