0bbfdfdb3db24c2c4e195cdb2e8943dc9541c54d
JojoGP215 <jojogp215@outlook.com>
2026-02-19 17:01:04 +0100

    Update EventController.php

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
-    public function index(): Response
+    public function __construct(private EventRepository $repository){}
+
+    #[Route('/events/{eventId}', name: 'app_events_id')]
+    public function index(int $eventId): Response
     {
-        return $this->render('event/index.html.twig', [
+        $event = $this->repository->find($eventId);
+
+        return $this->render('pages/event.html.twig', [
             'controller_name' => 'EventController',
+            'event'=> $event
         ]);
     }
 }
